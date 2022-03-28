cfe34e609ab5a3938e4f4d1787fd0a1c9d5f6c4d
Barani203 <naveenkumarmy203@gmail.com>
2022-03-28 20:24:12 +0530

    .

--- config/db.js
+++ config/db.js
@@ -1,19 +1,21 @@
-const mongoose = require('mongoose');
-const config = require('config');
-const db = config.get('mongoURI');
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURI");
+//mongoose return promises
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(db, {
-            useNewUrlParser: true,
-            useCreateIndex: true,
-            useFindAndModify: false
-        });
-        console.log('MongoDB Connected....');
-    } catch (error) {
-        console.error(error.message);
-        process.exit(1);
-    }
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB Connected..");
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
